0dd37782e5584af41600c7fc6e6c64ba68c16b16
LubovVseyaRussi <nomorequestionz@bk.ru>
2021-03-09 21:21:14 +0300

    Add all.

---
+++ Weather/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Lubov on 09.03.2021.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
